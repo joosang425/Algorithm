47f25d1039d552ea750c41d4447eb0c2ef266eca
JooSang <68217970+joosang425@users.noreply.github.com>
2022-04-07 15:22:50 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.01 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num) {
+    int answer = 0;
+    
+    while(1) {
+        if(num == 1)
+            break;
+        if(answer == 500) {
+            answer = -1;
+            break;
+        }
+        
+        if(num % 2 == 0) 
+            num = num / 2;
+        else if(num % 2 == 1)
+            num = num * 3 + 1;
+        
+        answer++;
+    }
+    
+    return answer;
+}
