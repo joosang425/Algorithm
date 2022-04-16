8ead9da9f8fe5f5dad743a9c6df167bfb9edc31b
JooSang <68217970+joosang425@users.noreply.github.com>
2022-04-16 21:06:06 +0900

    Update 퇴사.cc

--- 백준/Silver/14501. 퇴사/퇴사.cc
+++ 백준/Silver/14501. 퇴사/퇴사.cc
@@ -1,44 +1,28 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int day_arr[16], price_arr[16];
-int dp[16], price[16];
+int n, result;
+int day[15], price[15], dp[15];
 
 int main() {
-	int N;
-	cin >> N;
+	ios::sync_with_stdio(0);
+	cin.tie(0);
 
-	for (int i = 1; i <= N; i++)
-		cin >> day_arr[i] >> price_arr[i];
-
-	dp[1] = day_arr[1] + 1;
-	price[1] = price_arr[1];
-
-	for (int i = 2; i <= N; i++) {
-		dp[i] = day_arr[i] + i;
-		int max = -123456789;
-		for (int j = i - 1; j >= 1; j--) {
-			if (dp[j] <= i) {
-				if (max < price[j])
-					max = price[j];
-			}
-		}
-		if (max == -123456789)
-			price[i] = price_arr[i];
-		else
-			price[i] = max + price_arr[i];
-	}
-
-
-	int result = 0;
-	for (int i = 1; i <= N; i++) {
-		if (dp[i] <= N + 1) {
-			if (result < price[i])
-				result = price[i];
+	cin >> n;
+	// n일 만큼 진행
+	for (int i = 1; i <= n; i++) {
+		cin >> day[i] >> price[i];
+		// 현재 일 - 1만큼 진행
+		for (int j = 0; j < i; j++) {
+			if (day[j] + j <= i && i + day[i] <= n + 1)
+				dp[i] = max(dp[i], dp[j] + price[i]);
 		}
 	}
 
-	cout << result << endl;
+	for (int i = 0; i <= n; i++)
+		result = max(result, dp[i]);
+
+	cout << result << '\n';
 
 	return 0;
 }
