f19c478be3ed15e7e0a27ff196658e977a17481a
JooSang <68217970+joosang425@users.noreply.github.com>
2022-04-18 22:00:58 +0900

    Update 로봇 청소기.cc

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.cc
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.cc
@@ -1,39 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int MAP[51][51];
-int n, m, ry, rx, rd, cnt;
+int n, m, result, arr[50][50];
+pair<pair<int, int>, int> robot;
 
-int dir[4][2] = { {-1,0}, {0,1},{1,0},{0,-1} };
+// 북, 동, 남, 서
+int dir[4][2] = { {-1,0},{0,1},{1,0},{0,-1} };
 
-int solve(int y, int x, int d) {
-	if (!MAP[y][x]) {
-		MAP[y][x] = 2;
-		cnt++;
-	}
-
+bool isEmpty(int y, int x, int d) {
+	// 4번동안 반복하면서 빈 공간 찾기
 	for (int i = 0; i < 4; i++) {
-		int nd = (d + 3 - i) % 4;
-		int ny = y + dir[nd][0];
-		int nx = x + dir[nd][1];
+		d = (d + 3) % 4;	// +3한게 왼쪽 방향으로 회전한 것
+		int ny = y + dir[d][0];
+		int nx = x + dir[d][1];
 
 		if (ny < 0 || ny >= n || nx < 0 || nx >= m)
 			continue;
 
-		if (!MAP[ny][nx])
-			solve(ny, nx, nd);
+		if (!arr[ny][nx]) {
+			robot.first.first = ny;
+			robot.first.second = nx;
+			robot.second = d;
+			return true;
+		}
 	}
 
-	int nd = (d + 2) % 4;
-	int ny = y + dir[nd][0];
-	int nx = x + dir[nd][1];
-
-	if (MAP[ny][nx] == 1) {
-		cout << cnt << '\n';
-		exit(0);
-	}
-	else
-		solve(ny, nx, d);
+	return false;
 }
 
 int main() {
@@ -41,15 +33,44 @@ int main() {
 	cin.tie(0);
 
 	cin >> n >> m;
-	cin >> ry >> rx >> rd;
+
+	cin >> robot.first.first >> robot.first.second >> robot.second;
 
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
-			cin >> MAP[i][j];
+			cin >> arr[i][j];
 		}
 	}
 
-	solve(ry, rx, rd);
+	while (1) {
+		int y = robot.first.first;
+		int x = robot.first.second;
+		int d = robot.second;
+
+		// 현재 위치를 청소
+		if (!arr[y][x]) {
+			arr[y][x] = 2;
+			result++;
+		}
+
+		// 청소하지 않은 빈 공간을 찾지 못한 경우
+		if (!isEmpty(y, x, d)) {
+			int nd = (d + 2) % 4;
+			int ny = y + dir[nd][0];
+			int nx = x + dir[nd][1];
+
+			// 뒤가 벽인 경우 반복문 종료
+			if (arr[ny][nx] == 1)
+				break;
+			else {
+				robot.first.first = ny;
+				robot.first.second = nx;
+				robot.second = d; // 기존 방향 유지한 채 저장
+			}
+		}
+	}
+
+	cout << result << '\n';
 
 	return 0;
 }
