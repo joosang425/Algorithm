8186e2f36ef9bf526d3a4ec28af4d15bc5397b79
JooSang <68217970+joosang425@users.noreply.github.com>
2022-04-19 20:53:24 +0900

    Update 톱니바퀴.cc

--- 백준/Gold/14891. 톱니바퀴/톱니바퀴.cc
+++ 백준/Gold/14891. 톱니바퀴/톱니바퀴.cc
@@ -1,29 +1,60 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// N -> 0, S -> 1
+// -1 -> 반시계, 1 -> 시계
+
 string str;
-int k;
+int arr[4][8], k, num, dir, result;
+int isTurn[4];
 
-bool check[4];
-int remem[4];
-int chain[4][8];
+// 톱니바퀴가 도는지 확인하는 함수
+void check(int idx) {
+	int cur = idx;
 
-void turn(int cur) {
-	int temp = chain[cur][7];
+	// 현재 톱니바퀴에서 오른쪽으로
+	for (int i = idx + 1; i < 4; i++) {
+		// 같은 값을 가지면 그 즉시 종료
+		if (arr[cur][2] == arr[i][6]) 
+			break;
 
-	for (int i = 6; i >= 0; i--)
-		chain[cur][i + 1] = chain[cur][i];
+		// 아닌 경우 현재 톱니바퀴 반대 방향을 저장
+		isTurn[i] = isTurn[cur] * -1;
+		cur = i;
+	}
 
-	chain[cur][0] = temp;
+	// 현재 톱니바퀴에서 왼쪽으로
+	cur = idx;
+	for (int i = idx - 1; i >= 0; i--) {
+		if (arr[cur][6] == arr[i][2])
+			break;
+
+		isTurn[i] = isTurn[cur] * -1;
+		cur = i;
+	}
 }
 
-void re_turn(int cur) {
-	int temp = chain[cur][0];
+void move_(int idx) {
+	int dir = isTurn[idx];
 
-	for (int i = 1; i < 8; i++)
-		chain[cur][i - 1] = chain[cur][i];
+	// 회전 방향이 반시계
+	if (dir == -1) {
+		int temp = arr[idx][0];
 
-	chain[cur][7] = temp;
+		for (int i = 0; i < 7; i++)
+			arr[idx][i] = arr[idx][i + 1];
+
+		arr[idx][7] = temp;
+	}
+	// 회전 방향이 시계
+	else {
+		int temp = arr[idx][7];
+
+		for (int i = 7; i > 0; i--)
+			arr[idx][i] = arr[idx][i - 1];
+
+		arr[idx][0] = temp;
+	}
 }
 
 int main() {
@@ -32,63 +63,29 @@ int main() {
 
 	for (int i = 0; i < 4; i++) {
 		cin >> str;
-
-		for (int j = 0; j < 8; j++)
-			chain[i][j] = str[j] - '0';
+		for (int j = 0; j < str.length(); j++) {
+			arr[i][j] = str[j] - '0';
+		}
 	}
 
 	cin >> k;
-	int num1, num2;
-
-	//	1 = 시계 방향, -1 = 반시계 방향
-
 	for (int i = 0; i < k; i++) {
-		memset(check, false, sizeof(check));
-		memset(remem, 0, sizeof(remem));
+		cin >> num >> dir;
 
-		cin >> num1 >> num2;
+		memset(isTurn, 0, sizeof(isTurn));
 
-		num1--;
+		isTurn[num - 1] = dir;
+		check(num - 1);
 
-		remem[num1] = num2;
-		check[num1] = true;
-
-		for (int j = num1 - 1; j >= 0; j--) {
-			if (!check[j + 1])
-				continue;
-			if (chain[j + 1][6] == chain[j][2])
-				continue;
-
-			remem[j] = remem[j + 1] * -1;
-			check[j] = true;
-		}
-
-		for (int j = num1 + 1; j < 4; j++) {
-			if (!check[j - 1])
-				continue;
-			if (chain[j - 1][2] == chain[j][6])
-				continue;
-
-			remem[j] = remem[j - 1] * -1;
-			check[j] = true;
-		}
-
+		// 0이 아닌 경우에는 회전한다는 뜻
 		for (int j = 0; j < 4; j++) {
-			if (!check[j])
-				continue;
-
-			if (remem[j] == 1) 
-				turn(j);
-			else
-				re_turn(j);
+			if (isTurn[j] != 0)
+				move_(j);
 		}
 	}
 
-	//	N = 0, S = 1
-
-	int result = 0;
 	for (int i = 0; i < 4; i++) {
-		if (chain[i][0])
+		if (arr[i][0] == 1)
 			result += pow(2, i);
 	}
 
