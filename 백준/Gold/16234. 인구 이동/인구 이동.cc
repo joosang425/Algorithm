0f7878727cabc7046873f44039483b9f7deb07bd
JooSang <68217970+joosang425@users.noreply.github.com>
2022-04-21 16:35:53 +0900

    Update 인구 이동.cc

--- 백준/Gold/16234. 인구 이동/인구 이동.cc
+++ 백준/Gold/16234. 인구 이동/인구 이동.cc
@@ -1,67 +1,54 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, l, r, day, MAP[51][51];
-bool visited[51][51], check = true;
+int n, l, r, result, arr[51][51], temp[51][51];
+bool visited[51][51];
 
-int dir[4][2] = { {0,1},{0,-1},{1,0},{-1,0} };
+int dir[4][2] = { {-1,0},{0,1},{0,-1},{1,0} };
 
-void solve(int y, int x) {
+int bfs(int y, int x) {
+	int cnt = 1, sum = arr[y][x];
+	
+	// 큐를 2개 만든다 (bfs 진행을 위한 큐, 인구이동하는 칸 저장하는 큐)
 	queue<pair<int, int>> q, tq;
 	tq.push({ y,x });
 	q.push({ y,x });
 	visited[y][x] = true;
-	int sum = 0;
-	int cnt = 0;
 
 	while (!q.empty()) {
 		int cy = q.front().first;
 		int cx = q.front().second;
 		q.pop();
 
-		sum += MAP[cy][cx];
-		cnt += 1;
 		for (int i = 0; i < 4; i++) {
 			int ny = cy + dir[i][0];
 			int nx = cx + dir[i][1];
-			int temp = abs(MAP[cy][cx] - MAP[ny][nx]);
 
-			if (temp < l || temp > r)
-				continue;
-			if (ny < 0 || ny >= n || nx < 0 || nx >= n)
-				continue;
-			if (visited[ny][nx])
+			if (ny < 0 || ny >= n || nx < 0 || nx >= n || visited[ny][nx])
 				continue;
 
-
-			q.push({ ny,nx });
-			tq.push({ ny,nx });
-			visited[ny][nx] = true;
+			int value = abs(arr[cy][cx] - arr[ny][nx]);
+			
+			// 조건을 만족할 시 이동 및 큐에 저장
+			if (value >= l && value <= r) {
+				tq.push({ ny,nx });
+				q.push({ ny,nx });
+				cnt++;
+				sum += arr[ny][nx];
+				visited[ny][nx] = true;
+			}
 		}
 	}
 
+	// 저장해놓은 칸의 값을 변경
 	int value = sum / cnt;
 	while (!tq.empty()) {
-		MAP[tq.front().first][tq.front().second] = value;
+		arr[tq.front().first][tq.front().second] = value;
 		tq.pop();
 	}
-}
 
-bool cancombi(int y, int x) {
-	for (int i = 0; i < 4; i++) {
-		int ny = y + dir[i][0];
-		int nx = x + dir[i][1];
-		int temp = abs(MAP[y][x] - MAP[ny][nx]);
-
-		if (ny < 0 || ny >= n || nx < 0 || nx >= n)
-			continue;
-		if (temp < l || temp > r)
-			continue;
-
-		return true;
-	}
-
-	return false;
+	// 인구이동했는지 확인을 위한 return 값
+	return cnt;
 }
 
 int main() {
@@ -71,29 +58,33 @@ int main() {
 	cin >> n >> l >> r;
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
-			cin >> MAP[i][j];
+			cin >> arr[i][j];
 		}
 	}
 
-	while (check) {
-		check = false;
-
+	while (1) {
+		memset(visited, false, sizeof(visited));
+		bool flag = false;
+		
+		// bfs를 진행하면서 인구 이동가능한 지역 확인하기
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
-				if (!visited[i][j] && cancombi(i, j)) {
-					solve(i, j);
-					check = true;
+				if (!visited[i][j]) {
+					// bfs를 진행한 뒤 return값이 1보다 작다는 것은 이동할 수 있는 지역이 없다는 뜻이므로 1보다 크면 인구이동 했다고 설정
+					if (bfs(i, j) > 1)
+						flag = true;
 				}
 			}
 		}
+		
+		// 인구이동이 없으면 반복문 
+		if (!flag)
+			break;
 
-		if (check)
-			day++;
-
-		memset(visited, false, sizeof(visited));
+		result++;
 	}
 
-	cout << day << '\n';
+	cout << result << '\n';
 
 	return 0;
 }
