6157d727222199f28dac129e7ef0004329ba4360
JooSang <68217970+joosang425@users.noreply.github.com>
2022-04-22 20:28:03 +0900

    Update 이차원 배열과 연산.cc

--- 백준/Gold/17140. 이차원 배열과 연산/이차원 배열과 연산.cc
+++ 백준/Gold/17140. 이차원 배열과 연산/이차원 배열과 연산.cc
@@ -1,87 +1,85 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int r, c, k, r_cnt, c_cnt, result;
-
-int arr[101][101];
+int r, c, k, result, row_size, col_size, arr[100][100];
 
 bool cmp(pair<int, int> a, pair<int, int> b) {
-	if (a.second < b.second)
-		return true;
-	else if (a.second == b.second) {
-		if (a.first < b.first)
-			return true;
-	}
+	if (a.second == b.second)
+		return a.first < b.first;
 
-	return false;
+	return a.second < b.second;
 }
 
-void solve() {
-	if (r_cnt >= c_cnt) {
-		int max_size = 0;
+// R연산
+void row_cal() {
+	int max_col = 0;
 
-		for (int i = 1; i <= r_cnt; i++) {
-			vector<pair<int, int>> v;
-			int temp[101] = { 0, };
+	for (int i = 0; i < row_size; i++) {
+		map<int, int> m;
 
-			for (int j = 1; j <= c_cnt; j++)
-				temp[arr[i][j]]++;
+		for (int j = 0; j < col_size; j++) {
+			// 0은 무시
+			if (arr[i][j] == 0)
+				continue;
 
-			for (int j = 1; j <= 100; j++) {
-				if (temp[j] != 0)
-					v.push_back({ j, temp[j] });
-			}
-
-			sort(v.begin(), v.end(), cmp);
-
-			for (int j = 1; j <= c_cnt; j++)
-				arr[i][j] = 0;
-
-			int idx = 1;
-			for (int j = 0; j < v.size(); j++) {
-				if (idx > 100)
-					break;
-
-				arr[i][idx++] = v[j].first;
-				arr[i][idx++] = v[j].second;
-			}
-
-			max_size = max(max_size, idx - 1);
+			m[arr[i][j]]++;
 		}
 
-		c_cnt = max_size;
-	}
-	else {
-		int max_size = 0;
+		vector<pair<int, int>> v(m.begin(), m.end());
+		sort(v.begin(), v.end(), cmp);	// 조건에 맞게 정렬
 
-		for (int j = 1; j <= c_cnt; j++) {
-			vector<pair<int, int>> v;
-			int temp[101] = { 0, };
+		for (int j = 0; j < col_size; j++)
+			arr[i][j] = 0;
 
-			for (int i = 1; i <= r_cnt; i++)
-				temp[arr[i][j]]++;
+		int idx = 0;
+		for (int j = 0; j < v.size(); j++) {
+			// 100이상은 버림
+			if (idx > 99)
+				break;
 
-			for (int i = 1; i <= 100; i++) {
-				if (temp[i] != 0)
-					v.push_back({ i, temp[i] });
-			}
-
-			sort(v.begin(), v.end(), cmp);
-
-			for (int i = 1; i <= r_cnt; i++)
-				arr[i][j] = 0;
-
-			int idx = 1;
-			for (int i = 0; i < v.size(); i++) {
-				arr[idx++][j] = v[i].first;
-				arr[idx++][j] = v[i].second;
-			}
-
-			max_size = max(max_size, idx - 1);
+			arr[i][idx++] = v[j].first;
+			arr[i][idx++] = v[j].second;
 		}
 
-		r_cnt = max_size;
+		// 가장 큰 열의 크기를 알아야 함
+		max_col = max(max_col, idx);
 	}
+
+	col_size = max_col;
+}
+
+void col_cal() {
+	int max_row = 0;
+
+	for (int j = 0; j < col_size; j++) {
+		map<int, int> m;
+
+		for (int i = 0; i < row_size; i++) {
+			if (arr[i][j] == 0)
+				continue;
+
+			m[arr[i][j]]++;
+		}
+
+		vector<pair<int, int>> v(m.begin(), m.end());
+		sort(v.begin(), v.end(), cmp);
+
+		for (int i = 0; i < row_size; i++)
+			arr[i][j] = 0;
+
+		int idx = 0;
+		for (int i = 0; i < v.size(); i++) {
+			if (idx > 99)
+				break;
+
+			arr[idx++][j] = v[i].first;
+			arr[idx++][j] = v[i].second;
+		}
+
+		max_row = max(max_row, idx);
+	}
+
+	row_size = max_row;
 }
 
 int main() {
@@ -89,21 +87,25 @@ int main() {
 	cin.tie(0);
 
 	cin >> r >> c >> k;
-
-	r_cnt = c_cnt = 3;
-	
-	for (int i = 1; i <= 3; i++) {
-		for (int j = 1; j <= 3; j++) {
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
 			cin >> arr[i][j];
 		}
 	}
 
+	// 초기 크기는 3 X 3
+	row_size = col_size = 3;
+
 	while (1) {
-		if (arr[r][c] == k || result > 100)
+		// 조건에 만족하거나 100번 이상 실행되었을 경우 종료
+		if (arr[r - 1][c - 1] == k || result > 100)
 			break;
 
-		solve();
 		result++;
+		if (row_size >= col_size)
+			row_cal();
+		else
+			col_cal();
 	}
 
 	if (result > 100)
