676a9beeaade7638f64c7996f52d709ff2ff77be
JooSang <68217970+joosang425@users.noreply.github.com>
2022-04-25 18:58:13 +0900

    [Gold II] Title: 청소년 상어, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/19236. 청소년 상어/청소년 상어.cc
+++ 백준/Gold/19236. 청소년 상어/청소년 상어.cc
@@ -1,143 +1,104 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct fish {
+struct info {
 	int y;
 	int x;
 	int dir;
-	bool exist;
 };
 
+info fish[16];
 int a, b, result, arr[4][4];
-int direction[9][2] = { {0,0}, {-1,0}, {-1,-1}, {0,-1}, {1,-1},{1,0},{1,1},{0,1},{-1,1} };
 
-fish f[20];
+int d[8][2] = { {-1,0},{-1,-1},{0,-1},{1,-1},{1,0},{1,1},{0,1},{-1,1} };
 
-void exchange_fish(int idx, int nidx) {
-	fish temp = f[idx];
-	f[idx].y = f[nidx].y;
-	f[idx].x = f[nidx].x;
-	f[nidx].y = temp.y;
-	f[nidx].x = temp.x;
-}
-
-void fish_move() {
-	for (int i = 1; i <= 16; i++) {
-		if (!f[i].exist)
-			continue;
-
-		int y = f[i].y;
-		int x = f[i].x;
-		int dir = f[i].dir;
-
-		int ny = y + direction[dir][0];
-		int nx = x + direction[dir][1];
-
-		bool flag = false;
-
-		if (ny >= 0 && ny < 4 && nx >= 0 && nx < 4) {
-			if (arr[ny][nx] == 0) {
-				flag = true;
-				f[i].y = ny, f[i].x = nx;
-				arr[ny][nx] = i;
-				arr[y][x] = 0;
-			}
-			else if(arr[ny][nx] != -1){
-				flag = true;
-				exchange_fish(i, arr[ny][nx]);
-				swap(arr[y][x], arr[ny][nx]);
-			}
-		}
-
-		if (!flag) {
-			int ndir = dir + 1;
-			if (ndir == 9)
-				ndir = 1;
-
-			int ny = y + direction[ndir][0];
-			int nx = x + direction[ndir][1];
-
-			while (ndir != dir) {
-				if (ny >= 0 && ny < 4 && nx >= 0 && nx < 4) {
-					if (arr[ny][nx] == 0) {
-						f[i].y = ny;
-						f[i].x = nx;
-						arr[ny][nx] = i;
-						arr[y][x] = 0;
-						f[i].dir = ndir;
-						break;
-					}
-					else if (arr[ny][nx] != -1) {
-						exchange_fish(i, arr[ny][nx]);
-						swap(arr[y][x], arr[ny][nx]);
-						f[i].dir = ndir;
-						break;
-					}
-				}
-
-				ndir++;
-				if (ndir == 9)
-					ndir = 1;
-				ny = y + direction[ndir][0];
-				nx = x + direction[ndir][1];
-			}
-		}
-	}
-}
-
-void Copy(int arr1[][4], int arr2[][4], fish f1[], fish f2[]) {
+void copy(int arr1[][4], int arr2[][4], info fish1[], info fish2[]) {
 	for (int i = 0; i < 4; i++) {
 		for (int j = 0; j < 4; j++) {
 			arr1[i][j] = arr2[i][j];
 		}
 	}
 
-	for (int i = 1; i <= 16; i++)
-		f1[i] = f2[i];
+	for (int i = 0; i < 16; i++)
+		fish1[i] = fish2[i];
 }
 
-void setting(int y, int x, int ny, int nx, int idx, bool check) {
-	if (check) {
-		arr[y][x] = 0;
-		arr[ny][nx] = -1;
-		f[idx].exist = false;
-	}
-	else {
-		arr[y][x] = -1;
-		arr[ny][nx] = idx;
-		f[idx].exist = true;
-	}
-}
+void solve(int arr[][4], info fish[], int shark_y, int shark_x, int sum) {
+	// 백 트래킹을 위한 함수 
+	int temp_arr[4][4];
+	info temp_fish[16];
 
-void solve(int y, int x, int dir, int sum) {
+	copy(temp_arr, arr, temp_fish, fish);
+
+	// 물고기 먹기
+	int fish_num = temp_arr[shark_y][shark_x];
+	int shark_dir = temp_fish[fish_num].dir;
+
+	temp_arr[shark_y][shark_x] = -1;
+	temp_fish[fish_num].y = -1;
+	temp_fish[fish_num].x = -1;
+	temp_fish[fish_num].dir = -1;
+
+	sum += fish_num + 1;
 	result = max(result, sum);
 
-	int temp_arr[4][4];
-	fish temp_f[20];
-	Copy(temp_arr, arr, temp_f, f);
+	// 물고기 이동
+	for (int i = 0; i < 16; i++) {
+		// y가 -1이면 현재 물고기가 없다는 뜻
+		if (temp_fish[i].y == -1)
+			continue;
 
-	fish_move();
+		int cy = temp_fish[i].y;
+		int cx = temp_fish[i].x;
+		int cd = temp_fish[i].dir;
+		
+		int ny = cy + d[cd][0];
+		int nx = cx + d[cd][1];
+		int nd = cd;
 
-	for (int i = 1; i <= 3; i++) {
-		int ny = y + direction[dir][0] * i;
-		int nx = x + direction[dir][1] * i;
-
-		if (ny >= 0 && ny < 4 && nx >= 0 && nx < 4) {
-			if (arr[ny][nx] == 0)
-				continue;
-
-			int candi = arr[ny][nx];
-			int ndir = f[candi].dir;
-
-			setting(y, x, ny, nx, candi, true);
-			solve(ny, nx, ndir, sum + candi);
-			setting(y, x, ny, nx, candi, false);
+		// 이동할 수 있는 방향 찾기
+		while (ny < 0 || ny >= 4 || nx < 0 || nx >= 4 || (ny == shark_y && nx == shark_x)) {
+			nd = (nd + 1) % 8;
+			ny = cy + d[nd][0];
+			nx = cx + d[nd][1];
 		}
-		else
-			break;
+
+		// 현재 위치에 다른 물고기가 위치한 경우
+		if (temp_arr[ny][nx] != -1) {
+			int target = temp_arr[ny][nx];
+
+			temp_fish[target].y = cy;
+			temp_fish[target].x = cx;
+
+			temp_fish[i].y = ny;
+			temp_fish[i].x = nx;
+			temp_fish[i].dir = nd;
+
+			temp_arr[cy][cx] = target;
+			temp_arr[ny][nx] = i;
+		}
+		else {
+			temp_fish[i].y = ny;
+			temp_fish[i].x = nx;
+			temp_fish[i].dir = nd;
+
+			temp_arr[ny][nx] = i;
+			temp_arr[cy][cx] = -1;
+		}
 	}
 
-	Copy(arr, temp_arr, f, temp_f);
+	// 상어 이동
+	for (int i = 1; i < 4; i++) {
+		int ny = shark_y + d[shark_dir][0] * i;
+		int nx = shark_x + d[shark_dir][1] * i;
+
+		if (ny < 0 || ny >= 4 || nx < 0 || nx >= 4)
+			break;
+
+		// 물고기가 있을 때만 이동
+		if (temp_arr[ny][nx] != -1)
+			solve(temp_arr, temp_fish, ny, nx, sum);
+	}
 }
 
 int main() {
@@ -148,19 +109,16 @@ int main() {
 		for (int j = 0; j < 4; j++) {
 			cin >> a >> b;
 
+			a--, b--;
+
 			arr[i][j] = a;
-			f[a] = { i, j, b, true };
+			fish[a] = { i, j, b };
 		}
 	}
 
-	int cur = arr[0][0];
-	int dir = f[cur].dir;
-	f[cur].exist = false;
-	arr[0][0] = -1;
+	solve(arr, fish, 0, 0, 0);
 
-	solve(0, 0, dir, cur);
-
-	cout << result;
+	cout << result << '\n';
 
 	return 0;
 }
