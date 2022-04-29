0fe9b81da3a8ac8d1cb1704e2a240fe721fa7ec2
JooSang <68217970+joosang425@users.noreply.github.com>
2022-04-29 17:20:38 +0900

    Update 온풍기 안녕！.cc

--- 백준/Platinum/23289. 온풍기 안녕！/온풍기 안녕！.cc
+++ 백준/Platinum/23289. 온풍기 안녕！/온풍기 안녕！.cc
@@ -9,6 +9,7 @@ int left_temper_dir[3][2] = { {-1, -1}, {0, -1}, {1, -1} };
 int up_temper_dir[3][2] = { {-1, -1}, {-1, 0}, {-1, 1} };
 int down_temper_dir[3][2] = { {1, -1}, {1, 0}, {1, 1} };
 
+// 각 칸 별로 벽 여부 저정 -> 위쪽, 오른쪽, 아래쪽, 왼쪽순으로
 bool wall[20][20][4], visited[20][20];
 
 vector<pair<int, int>> machine, examine;
@@ -18,12 +19,13 @@ void spread_temper(int y, int x, int d, int value) {
 		visited[y][x] = true;
 		temper[y][x] += value;
 
-		return;
+		return;	// value값이 1이면 더 이상 3방향으로 진행할 필요 없으므로 방문 및 값 처리 후 return
 	}
 
 	visited[y][x] = true;
-	temper[y][x] += value;
+	temper[y][x] += value;	// 현재 갔던 칸은 더 이상 처리하지 않기 위해 visited 배열 true로 처리
 
+	// 바람을 3방향으로 진행시키면서 갈 수 있는 칸인지 확인 후 온도 값을 올려줌
 	if (d == 1) {
 		for (int i = 0; i < 3; i++) {
 			int ny = y + right_temper_dir[i][0];
@@ -123,29 +125,31 @@ void spread_temper(int y, int x, int d, int value) {
 
 void machine_on() {
 	for (int i = 0; i < machine.size(); i++) {
-		memset(visited, false, sizeof(visited));
+		memset(visited, false, sizeof(visited)); // 각 온풍기마다 바람이 갔던 칸은 넘기기 위한 배열 초기화
 
 		int y = machine[i].first;
 		int x = machine[i].second;
 		int d = arr[y][x];
 
-		spread_temper(y + dir[d][0], x + dir[d][1], d, 5);
+		spread_temper(y + dir[d][0], x + dir[d][1], d, 5);	// 3방향으로 퍼지는 바람 처리를 위한 함수 시작
 	}
 }
 
 void temper_control() {
-	int temp[20][20] = { 0, };
+	int temp[20][20] = { 0, };	// 모든 칸을 동시에 처리하기 위한 배열
 
 	for (int i = 0; i < r; i++) {
 		for (int j = 0; j < c; j++) {
 			int d = 0;
 			
+			// 인접한 방향의 온도 확인
 			for (int s = 0; s < 2; s++) {
-				d = s == 0 ? 1 : 4;
+				d = s == 0 ? 1 : 4;	// 오른쪽과 밑쪽으로만 진행 -> 상, 하, 좌, 우를 다 확인하면 중복으로 확인하는 거기 때문에 
 
 				int ny = i + dir[d][0];
 				int nx = j + dir[d][1];
 
+				// 갈 수 있는 칸인지 확인
 				if (ny < 0 || ny >= r || nx < 0 || nx >= c)
 					continue;
 
@@ -181,6 +185,8 @@ void temper_control() {
 }
 
 void edge_temper_control() {
+	// 가장 바깥쪽의 칸 온도 감소
+
 	for (int i = 0; i < r; i++) {
 		if (temper[i][0]) temper[i][0]--;
 		if (temper[i][c - 1]) temper[i][c - 1]--;
@@ -231,17 +237,17 @@ int main() {
 	}
 
 	while (1) {
-		machine_on();
-		temper_control();
-		edge_temper_control();
+		machine_on();	// 온풍기 바람 처리
+		temper_control();	// 온도가 조절
+		edge_temper_control();	// 바깥쪽 칸의 온도 감소
 
-		result++;
+		result++;	// 초콜릿 섭취
 		if (result > 100) {
 			result = 101;
 			break;
-		}
+		}	// 100보다 커지면 종료
 
-		if (examine_temper())
+		if (examine_temper())	// 조사하는 칸 온도 검사
 			break;
 	}
 
