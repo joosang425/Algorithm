5d03645bde92c2a0a80e94e25613e05dd84c55da
JooSang <68217970+joosang425@users.noreply.github.com>
2022-06-12 20:56:11 +0900

    [level 3] Title: 2 x n 타일링, Time: 0.28 ms, Memory: 3.94 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/12900. 2 x n 타일링/2 x n 타일링.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+int dp[60001];
+const int MOD = 1000000007;
+
+int solution(int n) {
+    int answer = 0;
+    
+    dp[0] = 0;
+    dp[1] = 1;
+    dp[2] = 2;
+    
+    for(int i = 3; i <= n; i++)
+        dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
+    
+    answer = dp[n];
+    
+    return answer;
+}
